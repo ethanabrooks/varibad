c964388fa2c05a66f08c017a23c6d4ac896029e1
ethanabrooks <ethanabrooks@gmail.com>
2023-04-28 11:57:02 -0400

    add --notes
    fix notes

--- main.py
+++ main.py
@@ -76,10 +76,12 @@ def parse_args():
     parser.add_argument("--env-type", default="gridworld_varibad")
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--replay_buffer", action="store_true")
+    parser.add_argument("--notes")
     args, rest_args = parser.parse_known_args()
     use_replay_buffer = args.replay_buffer
     debug = args.debug
     env = args.env_type
+    notes = args.notes
 
     # --- GridWorld ---
 
@@ -173,6 +175,7 @@ def parse_args():
 
     args.use_replay_buffer = use_replay_buffer
     args.debug = debug
+    args.notes = notes
     return args
 
 
@@ -231,6 +234,7 @@ def train(args, run: Optional[Run] = None):
             name=f"{args.env_name}-{args.exp_label}",
             sync_tensorboard=True,
             tags=get_tags(args.max_rollouts_per_task),
+            notes=args.notes,
         )
         run = wandb.run
 
@@ -269,6 +273,7 @@ def sweep(**config):
             project=args.project_name,
             rank_zero_only=False,
             tags=get_tags(args.max_rollouts_per_task),
+            notes=args.notes,
         )
         print(
             f"wandb: ️👪 View group at {run.get_project_url()}/groups/{urllib.parse.quote(group)}/workspace"
