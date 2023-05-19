c19b6d551d01b46f991e450dce1a2d617da1f03a
ethanabrooks <ethanabrooks@gmail.com>
2023-05-19 15:40:52 -0400

    update flake files from trajectory transformer

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/a983cc62cc2345443620597697015c1c9c4e5b06";
     utils.url = "github:numtide/flake-utils/93a2b84fc4b70d9e089d029deacc3583435c2ed6";
+    nixgl.url = "github:guibou/nixGL";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
+    nixgl,
   }: let
     out = system: let
       pkgs = import nixpkgs {
@@ -15,11 +17,31 @@
         config = {
           allowUnfree = true;
         };
+        overlays = [nixgl.overlay];
       };
       inherit (pkgs) poetry2nix;
-      mujoco = fetchTarball {
-        url = "https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz";
-        sha256 = "sha256:1lvppcdfca460sqnb0ryrach6lv1g9dwcjfim0dl4vmxg2ryaq7p";
+      mujoco = pkgs.stdenv.mkDerivation rec {
+        pname = "mujoco";
+        version = "2.1.0";
+
+        src = fetchTarball {
+          url = "https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz";
+          sha256 = "sha256:1lvppcdfca460sqnb0ryrach6lv1g9dwcjfim0dl4vmxg2ryaq7p";
+        };
+        installPhase = ''
+          install -d $out/bin
+          install -m 755 bin/* $out/bin/
+          install -d $out/include
+          install include/* $out/include/
+        '';
+        nativeBuildInputs = with pkgs;
+          [glew110 autoPatchelfHook]
+          ++ (with pkgs.xorg; [
+            libXcursor
+            libXinerama
+            libXrandr
+            libXxf86vm
+          ]);
       };
       overrides = pyfinal: pyprev: rec {
         mujoco-py =
@@ -28,17 +50,14 @@
           })
           .overridePythonAttrs (old: {
             env.NIX_CFLAGS_COMPILE = "-L${pkgs.mesa.osmesa}/lib";
-            preBuild = ''
+            preBuild = with pkgs; ''
               export MUJOCO_PY_MUJOCO_PATH="${mujoco}"
-              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib
+              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${mesa.osmesa}/lib:${libGL}/lib:${gcc-unwrapped.lib}/lib:${glew110}/lib
             '';
             buildInputs =
               old.buildInputs
-              ++ [
-                pyfinal.setuptools
-                pkgs.mesa
-                pkgs.libGL
-              ];
+              ++ [pyfinal.setuptools]
+              ++ (with pkgs; [mesa libGL]);
             patches = [./mujoco-py.patch];
           });
         torch = pyprev.pytorch-bin.overridePythonAttrs (old: {
@@ -57,13 +76,20 @@
         preferWheels = true;
         overrides = poetry2nix.overrides.withDefaults overrides;
       };
+
+      myNixgl = pkgs.nixgl.override {
+        nvidiaVersion = "510.108.03";
+        nvidiaHash = "sha256-QQpRXnjfKcLLpKwLSXiJzg/xsEz8cR/4ieLfyA8NoNg=";
+      };
     in {
       devShell = pkgs.mkShell {
-        LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.cudaPackages.cudnn}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib";
+        MUJOCO_PY_MUJOCO_PATH = "${mujoco}";
+        LD_LIBRARY_PATH = with pkgs; "$LD_LIBRARY_PATH:${mesa.osmesa}/lib:${gcc-unwrapped.lib}/lib:${mujoco}/bin";
         buildInputs = with pkgs; [
           alejandra
           poetry
           poetryEnv
+          myNixgl.nixGLNvidia
         ];
         PYTHONBREAKPOINT = "ipdb.set_trace";
         shellHook = ''
