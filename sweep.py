5d2035a9763ea67394557b557baf463cc309bc16
Ethan Brooks <ethanabrooks@gmail.com>
2023-05-27 21:44:17 -0400

    sweep over seed lists
    add --gpus-per-proc to sweep

--- sweep.py
+++ sweep.py
@@ -1,12 +1,22 @@
 from argparse import ArgumentParser
+
 from ray import tune
 
 from main import sweep
 
 parser = ArgumentParser()
-parser.add_argument("--num-seeds", type=int, default=8)
+parser.add_argument("--gpus-per-proc", type=float, default=0.5)
+parser.add_argument("--seq-len", type=int, default=16)
+parser.add_argument("--num-procs", type=int, default=16)
 args, rest_args = parser.parse_known_args()
 sweep(
-    config=dict(seed=tune.grid_search(list(range(args.num_seeds)))),
+    config=dict(
+        seed=tune.grid_search(
+            [
+                list(range(i, i + args.seq_len))
+                for i in range(0, args.seq_len * args.num_procs, args.seq_len)
+            ]
+        )
+    ),
     args=rest_args,
 )
