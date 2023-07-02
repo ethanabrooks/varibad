fce8accad5e539d9fad47fa4086a8843d32d18f0
ethanabrooks <ethanabrooks@gmail.com>
2023-07-01 21:31:19 -0400

    update sweep to handle --start-seed

--- sweep.py
+++ sweep.py
@@ -4,9 +4,12 @@ from ray import tune
 from main import sweep
 
 parser = ArgumentParser()
+parser.add_argument("--start-seed", type=int, default=0)
 parser.add_argument("--num-seeds", type=int, default=8)
 args, rest_args = parser.parse_known_args()
 sweep(
-    config=dict(seed=tune.grid_search(list(range(args.num_seeds)))),
+    config=dict(
+        seed=tune.grid_search([s + args.start_seed for s in range(args.num_seeds)])
+    ),
     args=rest_args,
 )
