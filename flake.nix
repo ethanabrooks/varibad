77075660d0b9be5676586550fcf5e6ec553d9848
ethanabrooks <ethanabrooks@gmail.com>
2023-08-20 15:56:20 -0400

    upgraded varibad nixGL by pulling all the deps files from trajectory-transformer

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/a983cc62cc2345443620597697015c1c9c4e5b06";
-    utils.url = "github:numtide/flake-utils/93a2b84fc4b70d9e089d029deacc3583435c2ed6";
+    nixpkgs.url = "github:nixos/nixpkgs/";
+    utils.url = "github:numtide/flake-utils/";
     nixgl.url = "github:guibou/nixGL";
   };
 
@@ -43,6 +43,8 @@
             libXxf86vm
           ]);
       };
+
+      python = pkgs.python39;
       overrides = pyfinal: pyprev: rec {
         dm-alchemy = pyprev.dm-alchemy.overridePythonAttrs (old: {
           buildInputs = old.buildInputs ++ (with pyfinal; [setuptools grpcio-tools]);
@@ -50,6 +52,29 @@
         grpcio-tools = pyprev.grpcio-tools.overridePythonAttrs (old: {
           buildInputs = [pyfinal.grpcio pkgs.gcc-unwrapped];
         });
+        mjrl = pyprev.buildPythonPackage {
+          pname = "mjrl";
+          version = "0.0.0";
+          buildInputs = with pyfinal; [
+            setuptools
+            gym
+            six
+            mujoco-py
+          ];
+          src = fetchGit {
+            url = "https://github.com/aravindr93/mjrl";
+            rev = "3871d93763d3b49c4741e6daeaebbc605fe140dc";
+          };
+          doCheck = false;
+        };
+        d4rl =
+          (pyprev.d4rl.override {
+            preferWheel = false;
+          })
+          .overridePythonAttrs (old: {
+            buildInputs = old.buildInputs ++ [pyfinal.mjrl];
+            patches = [./d4rl.patch];
+          });
         mujoco-py =
           (pyprev.mujoco-py.override {
             preferWheel = false;
@@ -58,7 +83,7 @@
             env.NIX_CFLAGS_COMPILE = "-L${pkgs.mesa.osmesa}/lib";
             preBuild = with pkgs; ''
               export MUJOCO_PY_MUJOCO_PATH="${mujoco}"
-              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib
+              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${mujoco}/bin:${mesa.osmesa}/lib:${libGL}/lib:${gcc-unwrapped.lib}/lib:${glew110}/lib
             '';
             buildInputs =
               old.buildInputs
@@ -66,55 +91,45 @@
               ++ (with pkgs; [mesa libGL]);
             patches = [./mujoco-py.patch];
           });
-        torch = pyprev.pytorch-bin.overridePythonAttrs (old: {
+        # Based on https://github.com/NixOS/nixpkgs/blob/nixos-22.11/pkgs/development/python-modules/torch/bin.nix#L107
+        torch = pyprev.buildPythonPackage {
+          version = "1.13.1";
+
+          pname = "torch";
+          # Don't forget to update torch to the same version.
+
+          format = "wheel";
+
           src = pkgs.fetchurl {
             url = "https://download.pytorch.org/whl/cu116/torch-1.13.1%2Bcu116-cp39-cp39-linux_x86_64.whl";
             sha256 = "sha256-20V6gi1zYBO2/+UJBTABvJGL3Xj+aJZ7YF9TmEqa+sU=";
           };
-        });
-        torchrl = pyprev.buildPythonPackage {
-          pname = "torchrl";
-          version = "0.0.5";
-          propagatedBuildInputs = with pyfinal; [
-            tensordict
-          ];
-          nativeBuildInputs = with pyfinal; [
-            setuptools
-            cloudpickle
-            packaging
-            pkgs.which
-          ];
-          src = pkgs.fetchgit {
-            url = "https://github.com/pytorch/rl";
-            rev = "48eca9890e24a64b0f3ee133a74abafad7cd0768";
-            sha256 = "sha256-ZLHqKRO7O4ZOSPqtwVmyFElpRnEbhX97Iy8PkGh4Fqg=";
-            name = "torchrl";
-          };
-          doCheck = false;
-          patches = [./torchrl.patch];
         };
+        torchrl = pyprev.torchrl.overridePythonAttrs (old: {
+          preFixup = "addAutoPatchelfSearchPath ${pyfinal.torch}";
+        });
       };
       poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-        python = pkgs.python39;
+        inherit python;
         projectDir = ./.;
         preferWheels = true;
         overrides = poetry2nix.overrides.withDefaults overrides;
       };
 
       myNixgl = pkgs.nixgl.override {
-        nvidiaVersion = "510.108.03";
-        nvidiaHash = "sha256-QQpRXnjfKcLLpKwLSXiJzg/xsEz8cR/4ieLfyA8NoNg=";
+        nvidiaVersion = "535.86.05";
+        nvidiaHash = "sha256-QH3wyjZjLr2Fj8YtpbixJP/DvM7VAzgXusnCcaI69ts=";
       };
     in {
       devShell = pkgs.mkShell {
         MUJOCO_PY_MUJOCO_PATH = "${mujoco}";
         MUJOCO_PY_FORCE_CPU = 1;
-        PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION = "python";
-        LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.cudaPackages.cudnn}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.mesa.osmesa}/lib:${pkgs.libGL}/lib:${pkgs.gcc-unwrapped.lib}/lib:${mujoco}/bin";
+        LD_LIBRARY_PATH = with pkgs; "$LD_LIBRARY_PATH:${mesa.osmesa}/lib:${gcc-unwrapped.lib}/lib:${mujoco}/bin";
         buildInputs = with pkgs; [
           alejandra
           poetry
           poetryEnv
+          ffmpeg
           myNixgl.nixGLNvidia
         ];
         PYTHONBREAKPOINT = "ipdb.set_trace";
